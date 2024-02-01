65285b0d67fd5e8c2d6325eac4f791b40993e080
Emre Can Temur | Yorgun Coder <71280682+w0fly@users.noreply.github.com>
2024-02-01 22:23:29 +0300

    DB Engine Revision

--- core/db.php
+++ core/db.php
@@ -1,3 +1,17 @@
 <?php
 
+$DBHOST = "localhost";
+$DBUSER = "root";
+$DBPASS = "";
+$DBNAME = "system";
+$DBPORT = "3306";
+
+try {
+    $con = new PDO("mysql:host=$DBHOST;port=$DBPORT;dbname=$DBNAME", $DBUSER, $DBPASS);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $con->exec("set names utf8");
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+
 ?>
