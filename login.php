2e68327afe609020b4e8a4d68fc89d3a88c7b43c
w0fly <o214233034@giresun.edu.tr>
2024-02-01 22:44:09 +0300

    UI Revision

--- login.php
+++ login.php
@@ -89,10 +89,10 @@ if (isset($_SESSION['giris']))
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="/plugins/jquery/jquery.min.js"></script>
+<script src="plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="/dist/js/adminlte.min.js"></script>
+<script src="dist/js/adminlte.min.js"></script>
 </body>
 </html>
